acc4cc9a8be39092b6e56249b5af7d03f632b649
jonichi16 <vvjhonel@gmail.com>
2023-02-15 00:26:32 +0800

    Change the login method from email only to username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validate :validate_username
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
