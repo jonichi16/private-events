8b826594904b378677b97249337bba2ffdf35259
jonichi16 <vvjhonel@gmail.com>
2023-02-14 12:48:47 +0800

    Add Event model

---
+++ db/migrate/20230214044321_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :description
+      t.datetime :date_of_event
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
