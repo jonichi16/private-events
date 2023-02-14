0e85dbe78b9c0fc37eb135d88857cb612ef1236b
jonichi16 <vvjhonel@gmail.com>
2023-02-15 02:00:18 +0800

    Create a form for new action of Events with validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :date_of_event, presence: true
+  validates :location, presence: true
 end
