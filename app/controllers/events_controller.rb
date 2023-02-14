93211d0f7a938556bc42b7816648b26dec20be38
jonichi16 <vvjhonel@gmail.com>
2023-02-15 01:47:26 +0800

    Update Eventcontroller for creating a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,25 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @creator = @event.build_creator(current_user)
+  
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :data_of_event, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  resources :events
   root 'events#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
