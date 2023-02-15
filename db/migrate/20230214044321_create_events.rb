f5ba5afd34e7f843651afefe2d3d414369a2f0ba
jonichi16 <vvjhonel@gmail.com>
2023-02-15 12:27:18 +0800

    Edit schema for event description to text and creator_id to accept null value

--- db/migrate/20230214044321_create_events.rb
+++ db/migrate/20230214044321_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.datetime :date_of_event
       t.string :location
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_170350) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.datetime "date_of_event"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "creator_id", null: false
+    t.integer "creator_id"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
