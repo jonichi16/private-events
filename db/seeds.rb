eb953c81eb1a360d12e7ee3550136556c2e4ca30
jonichi16 <vvjhonel@gmail.com>
2023-02-17 00:54:18 +0800

    Create seed to populate the users and events table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+  User.create(
+    email: "user#{i+1}@user.com",
+    password: "111111",
+    password_confirmation: "111111",
+    username: "user_#{i+1}",
+    first_name: "First #{i+1}",
+    last_name: "Last #{i+1}"
+  )
+  p "User#{i+1} created!"
+end
+
+10.times do |i|
+  Event.create(
+    title: "Event #{i+1}",
+    description: "Some Event",
+    date_of_event: Time.now + rand(-5..5).day,
+    location: "Somewhere",
+    creator_id: rand(0..9)
+  )
+  p "Event#{i+1} created!"
+end
