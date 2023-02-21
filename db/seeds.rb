c09ed8e685f6799760248bb31b8422084f717333
Jhonel Villanueva <vvjhonel@gmail.com>
2023-02-21 14:12:28 +0800

    feat: Edit seeds for better example

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,21 @@
     email: "user#{i+1}@user.com",
     password: "111111",
     password_confirmation: "111111",
-    username: "user_#{i+1}",
-    first_name: "First #{i+1}",
-    last_name: "Last #{i+1}"
+    username: "user#{i+1}",
+    first_name: "FirstName",
+    last_name: "LastName #{i+1}"
   )
   p "User#{i+1} created!"
 end
 
 10.times do |i|
   Event.create(
-    title: "Event #{i+1}",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a massa est. Duis sit amet feugiat urna. Quisque at aliquet sapien, at egestas arcu. Nulla egestas ex nibh, eu eleifend ex blandit at. Nullam at condimentum sapien. In hac habitasse platea dictumst. Etiam gravida nisi non volutpat feugiat. Aenean iaculis, ex ac tincidunt dignissim, nisl sem sagittis urna, ac auctor justo justo ut nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque quis varius tellus. Nunc posuere mauris eu erat finibus, ac faucibus arcu cursus. Ut pellentesque magna nisl, ut tincidunt lacus vestibulum et. Suspendisse ultrices vehicula lorem a scelerisque.
+    title: "Event #{i+1} Nunc posuere mauris eu",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a massa est. Duis sit amet feugiat urna. Quisque at aliquet sapien, at egestas arcu. Nulla egestas ex nibh, eu eleifend ex blandit at. Nullam at condimentum sapien. In hac habitasse platea dictumst. Etiam gravida nisi non volutpat feugiat. Aenean iaculis, ex ac tincidunt dignissim, nisl sem sagittis urna, ac auctor justo justo ut nisl. 
     
-    Duis tempus id lacus vitae pretium. Sed ac sem ipsum. Sed enim augue, molestie feugiat lobortis quis, convallis id ante. In vel ante consectetur, fringilla neque vitae, convallis urna. Nam ante diam, consectetur ac lectus sed, vehicula sodales neque. Curabitur id gravida dolor. Donec ut elit augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam a mi odio. Cras convallis sapien ut turpis tincidunt, ac molestie lorem eleifend. Vestibulum vitae facilisis eros. Proin malesuada pretium faucibus. Fusce nunc metus, posuere eu rutrum id, suscipit et ex. Morbi facilisis fermentum laoreet. Integer ultricies rhoncus neque, quis faucibus felis volutpat vel. Quisque dictum sem nibh, sit amet vehicula neque molestie eget.",
+    Duis tempus id lacus vitae pretium. Sed ac sem ipsum. Sed enim augue, molestie feugiat lobortis quis, convallis id ante. In vel ante consectetur, fringilla neque vitae, convallis urna. Nam ante diam, consectetur ac lectus sed, vehicula sodales neque. Curabitur id gravida dolor. Donec ut elit augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
     date_of_event: Time.now + rand(-5..10).day,
-    location: "Somewhere",
+    location: "Location Here",
     creator_id: rand(0..9)
   )
   p "Event#{i+1} created!"
